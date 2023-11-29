a509b919984b465665c086281c9924d1870b9e7c
premsai-noolu <premsainoolu01@gmail.com>
2023-11-29 11:00:27 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React!"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using jsx
+  </h1>
 );
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
---
+++ README.md
@@ -0,0 +1,21 @@
+# Namaste React 🚀
+
+# Parcel
+
+-Dev Build
+-Local Server
+-HMR = Hot module replacement
+
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Minification
+- Bundling
+- Compress
+- consistent Hashing
+- code splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- Https
+- Tree shaking - removed unused code
+- Different dev and prod bundles
