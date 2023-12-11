bd7bd1b6f3708ea340254f45d0b76f469e6a1881
premsai-noolu <premsainoolu01@gmail.com>
2023-12-11 10:19:17 +0530

    Tailwind css added

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,26 +14,26 @@ const Header = () => {
     console.log("Header render");
   }, []);
   return (
-    <div className="header">
+    <div className="flex justify-between shadow-lg bg-pink-50 sm:bg-yellow-100 lg:bg-green-100">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+        <img className="w-56" src={LOGO_URL} />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>Online Status : {onlineStatus ? "🟢" : "🔴"}</li>
-          <li>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4">Online Status : {onlineStatus ? "🟢" : "🔴"}</li>
+          <li className="px-4">
             <Link to="./">Home</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="./About">About Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="./Contact">Contact Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="./grocery">Grocery</Link>
           </li>
-          <li>Cart</li>
+          <li className="px-4">Cart</li>
           <button
             className="login"
             onClick={() => {
